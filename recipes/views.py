0c69401fd4023e667f2d407c6153a2197f635f04
Wemerson Gomes <106920230+WemersonGomesDev@users.noreply.github.com>
2022-10-28 08:58:57 -0300

    Commit start

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
+
+def home(request):
+    return render(request, 'recipes/pages/home.html', context= {
+            'recipes': [make_recipe() for _ in range(10)],
+    })
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context= {
+            'recipe': make_recipe(),
+    })
